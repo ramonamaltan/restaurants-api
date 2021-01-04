61e00802ebd05a4a333e95aff3cbbe7024fda86f
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-04 12:17:32 +0100

    added simple token authentication gem, users  migration and setup

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+
   def index
     @restaurants = policy_scope(Restaurant)
   end
