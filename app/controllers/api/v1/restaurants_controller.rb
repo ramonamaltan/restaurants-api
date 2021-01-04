e25be7991aa7c1985b6da9592291d24bae23e16e
Ramona Maltan <ramonamaltan@gmail.com>
2021-01-04 15:50:05 +0100

    added create action and policy

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User, except: [ :index, :show ]
   before_action :set_restaurant, only: [ :show, :update ]
-  
+
   def index
     @restaurants = policy_scope(Restaurant)
   end
@@ -13,6 +13,17 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
     @restaurant.update(restaurant_params)
     if @restaurant.update(restaurant_params)
       render :show
+    else
+      render_error
+    end
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+    authorize @restaurant
+    if @restaurant.save
+      render :show, status: :created
     else
       render_error
     end
--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -13,4 +13,9 @@ class RestaurantPolicy < ApplicationPolicy
     # only restaurant owners can update it
     record.user == user
   end
+
+  def create?
+    # any logged in user can create a restaurant
+    !user.nil?
+  end
 end
